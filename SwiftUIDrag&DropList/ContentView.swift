e02aa82fc49b91cc3937cca80532245f5ec7ddf9
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-03-26 18:45:56 +0200

    init

--- SwiftUIDrag&DropList/ContentView.swift
+++ SwiftUIDrag&DropList/ContentView.swift
@@ -9,8 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        UITableView.appearance().tableFooterView = UIView()
+        UITableView.appearance().separatorStyle = .none
+    }
+    
+    @State var recipeSteps = [
+        "One",
+        "Two",
+        "Three",
+        "Four",
+        "Five"
+    ]
+    
+    @State var editingList = false
+    
     var body: some View {
-        Text("Hello, World!")
+        List {
+            ForEach(0..<recipeSteps.count) { i in
+                Text("\(i): \(self.recipeSteps[i])")
+                    .padding(EdgeInsets.init(top: 3, leading: 6, bottom: 3, trailing: 6))
+                    .background(Color.init(red: 0.9, green: 0.9, blue: 0.9))
+                    .cornerRadius(5)
+                    .lineLimit(nil)
+            }
+            .onMove(perform: move)
+            .onLongPressGesture {
+                withAnimation {
+                    self.editingList = true
+                }
+            }
+        }.environment(\.editMode, editingList ? .constant(.active) : .constant(.inactive))
+    }
+    
+    func move(fromOffsets source: IndexSet, toOffsets destination: Int) {
+        recipeSteps.move(fromOffsets: source, toOffset: destination)
+        withAnimation {
+            editingList = false
+        }
     }
 }
 
